2fb7bb190d34002e59623da2d299a88859486690
louiselin <louise360992@gmail.com>
2016-12-16 16:04:19 +0800

    Pop-up of verify at Ledger page.

--- mildraledger/config/database.js
+++ mildraledger/config/database.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     connection: {
-        host: 'xxxx',
-        user: 'xxxx',
-        password: 'xxxx'
+        host: '140.119.164.152',
+        user: 'fallse2016',
+        password: '5487'
     },
     database: 'mildraledger',
     usersTable: 'users',
